0fb050c9dc638951183ebd7166cbfbcf9f58ff77
Alexander Filimonov <filimonov.alexander.s@yandex.ru>
2023-04-21 21:52:37 +0300

    refactor(basic-3.4): change tags selectors applying basic rules 3.1 and 3.8

--- app.js
+++ app.js
@@ -32,19 +32,23 @@ var createNewTaskElement=function(taskString){
     var deleteButton=document.createElement("button");//delete button
     var deleteButtonImg=document.createElement("img");//delete button image
 
+    listItem.className="task-block";
+    
     label.innerText=taskString;
-    label.className='task';
+    label.className='task-name';
 
     //Each elements, needs appending
     checkBox.type="checkbox";
+    checkBox.className="task-check";
     editInput.type="text";
-    editInput.className="task";
+    editInput.className="listed-task";
 
     editButton.innerText="Edit"; //innerText encodes special characters, HTML does not.
     editButton.className="edit";
 
     deleteButton.className="delete";
     deleteButtonImg.src='./remove.svg';
+    deleteButtonImg.className="delete-icon";
     deleteButton.appendChild(deleteButtonImg);
 
 
